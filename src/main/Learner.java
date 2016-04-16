82bc6836aaade543ad75827a2a2bec9979a3a07f
DongWei <leedongwei@gmail.com>
2016-04-16 17:01:23 +0800

    trying out new GA

--- src/main/Learner.java
+++ src/main/Learner.java
@@ -1,7 +1,9 @@
 package main;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.logging.FileHandler;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
@@ -11,16 +13,23 @@ public class Learner {
 
     public static int NUM_WEIGHTS = Features.NUM_FEATURES;
     public static Logger logger = Logger.getGlobal();
-    public static int GAMES_COUNT = 20;
+    public static int GAMES_COUNT = 50;
     public static double TOP_PERFORMERS = 1.0;
     private double[][] featureWeights;
+    
+    // last best score: 2473.2
+    // double[] lastBestScore = {107586.63663457146, 750753.4845484055, -150417.96616116664, -208169.70729578854, 2731.960240795902, -651269.0404827954, -66463.07469214208, -146345.66859520436};
+	
     
     private void initialiseFeatureWeights() {
         featureWeights = new double[GAMES_COUNT][NUM_WEIGHTS];
         for (int i = 0; i < GAMES_COUNT; i++) {
+//        	if (i < 5) { // put 5 of last score to ensure survival
+//        		featureWeights[i] = lastBestScore;
+//        	}
             for (int j = 0; j < NUM_WEIGHTS; j++) {
                 int negativeMultiplier = (Math.random() > 0.5) ? -1 : 1;
-                featureWeights[i][j] = negativeMultiplier * Math.random() * 1000000;
+                featureWeights[i][j] = negativeMultiplier * Math.random() * 500;
             }
         }
     }
@@ -63,11 +72,18 @@ public class Learner {
     
     // crossover
     private double[] reproduce(double[] featureWeightsOne, double[] featureWeightsTwo) {
-        int crossOverPoint = (int) Math.floor(Math.random() * NUM_WEIGHTS);
         double[] childFeatureWeights = new double[NUM_WEIGHTS];
+        
         for (int i = 0; i < NUM_WEIGHTS; i++) {
-            childFeatureWeights[i] = (i > crossOverPoint) ? featureWeightsTwo[i] : featureWeightsOne[i];
+            double randNum = Math.random();
+            if (randNum < 0.5) {
+            	childFeatureWeights[i] = featureWeightsOne[i];
+            } else {
+            	childFeatureWeights[i] = featureWeightsTwo[i];
+            }
         }
+
+        
         
         return childFeatureWeights;
     }
@@ -77,7 +93,7 @@ public class Learner {
         for (int i = 0; i < NUM_WEIGHTS; i++) {
             if (Math.random() < mutationProbability) {
                 int negativeMultiplier = (Math.random() > 0.5) ? -1 : 1;
-                featureWeights[i] = negativeMultiplier * Math.random() * 1000000;
+                featureWeights[i] = negativeMultiplier * Math.random() * 500;
             }
         }
         
@@ -90,18 +106,68 @@ public class Learner {
     }
     
     private void updateWeights(double[] gameScores) {
-        // select new weights based on performance
+        int[] chosenIndexes = new int[5];
+        ArrayList<double[]> newFeatureWeights = new ArrayList<double[]>();
 
-        //double cutOffScore = getCutOffScore(gameScores);
-        double cutOffScore = 0.0;
-        //System.out.println("cutOffScore: " + cutOffScore);
-        for (int i = 0; i < GAMES_COUNT; i++) {
-            int chosenIndex = getRandomIndex(gameScores, cutOffScore);
-            int chosenIndex2 = getRandomIndex(gameScores, cutOffScore);
-            double[] childFeatureWeights = reproduce(featureWeights[chosenIndex], featureWeights[chosenIndex2]);
-            childFeatureWeights = mutate(childFeatureWeights);
-            featureWeights[i] = childFeatureWeights;
+        while(newFeatureWeights.size() < GAMES_COUNT) {
+	        // sample random group of 5
+	        ArrayList<Integer> list = new ArrayList<Integer>();
+	        for (int j=0; j<GAMES_COUNT; j++) {
+	            list.add(new Integer(j));
+	        }
+	        Collections.shuffle(list);
+	        for (int j=0; j<5; j++) {
+	            chosenIndexes[j] = list.get(j);
+	        }
+	        
+	        // find highest scoring 2
+	        int highScoreIndex1 = -1,
+	        	highScoreIndex2 = -1;
+	        for (int i=0; i < 5; i++) {
+	        	if (highScoreIndex1 == -1) {
+	        		highScoreIndex1 = chosenIndexes[i];
+	        		continue;
+	        	} else {
+	        		if (gameScores[chosenIndexes[i]] > gameScores[highScoreIndex1]) {
+	        			highScoreIndex2 = highScoreIndex1;
+	        			highScoreIndex1 = chosenIndexes[i];
+	        			continue;
+	        		}
+	        	}
+	        	
+	        	if (highScoreIndex2 == -1) {
+	        		highScoreIndex2 = chosenIndexes[i];
+	        		continue;
+	        	} else {
+	        		if (gameScores[chosenIndexes[i]] > gameScores[highScoreIndex2]) {
+	        			highScoreIndex2 = chosenIndexes[i];
+	        			continue;
+	        		}
+	        	} 
+                
+	        }
+	        
+	        // reproduce 5 times
+            double[] childFeatureWeights;
+	        for (int i=0; i < 5; i++) {
+	        	childFeatureWeights = reproduce(
+	        			featureWeights[highScoreIndex1], 
+	        			featureWeights[highScoreIndex2]);
+	            newFeatureWeights.add(childFeatureWeights);
+	        }
+
+	        
+	        
+	       
         }
+        
+        double[] newweight;
+        for (int i=0; i < GAMES_COUNT; i++) {
+        	newweight = newFeatureWeights.remove(0);
+        	newweight = mutate(newweight);
+        	featureWeights[i] = newweight;
+        }
+
     }
     
     public void printAllFeatureWeights() {
@@ -133,7 +199,7 @@ public class Learner {
                 long time = System.nanoTime()/1000000;
                 double bestScoreOfCycle = 0;
                 logger.info("Starting cycle " + cycleNo);
-                //learner.printAllFeatureWeights();
+                // learner.printAllFeatureWeights();
                 String gameResults = "Games: "; 
                 double[] averageGameScores = new double[GAMES_COUNT];
                 final double[] gameFeatureWeights = new double[NUM_WEIGHTS];
@@ -196,6 +262,8 @@ public class Learner {
 
                 long time2 = System.nanoTime()/1000000;
                 System.out.println("Total time for cycle " + cycleNo +": " + (time2-time) + "ms.");
+                System.out.println("    Best score for CURRENT cycle: " + bestScoreOfCycle);
+                System.out.println("    Current best score and weights: " + bestScore + " and " + Arrays.toString(bestFeatureWeights) + " from cycle " + cycleOfBestScore);
                 cycleNo++;
             }
         } catch (SecurityException e) {
